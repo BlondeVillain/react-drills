bacd479a831a34658ff9e3c04d899b65601d9030
BlondeVillain <bradley.shadowens@gmail.com>
2018-08-15 18:21:44 -0500

    FunctionalApp2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+export default class App extends Component {
+  constructor() {
+    super();
+      this.state = {
+        filterString: "",
+        array: ["earth", "fire","wind","water","heart","GO PLANET!"]}}
+    
+  handleChange(filter) {
+  this.setState({filterString: filter})}
+  render() {
 
-class App extends Component {
-constructor() {
-super();
-  this.state = { captainplanet: ["earth", "fire","wind","water","heart","GO PLANET!"]}
+  let planeteers = this.state.array.filter( (element, index) => 
+      
+    {return element.includes(this.state.filterString)}).map(  (element, index)=>
+        
+      {return(<h1 key = {index}>{element}</h1>)})
+
+  return (<div className="App">
+  <input onChange={(e) => this.handleChange( e.target.value ) } type="text" />
+  { planeteers }
+  </div>)
   }
-render() {
-  let planeteers = this.state.captainplanet.map( (element, index) => {
-  return (<h1 key={ index }>{ element }</h1>)})
-  return (<div className="App">{ planeteers }</div>);}
 }
-
-export default App;
